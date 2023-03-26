073c33b2b309d47168d79637ec1c8675efb774b4
forellko <dragonforce97q@gmail.com>
2023-03-26 16:42:27 +0300

    Add Product component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Product } from './components/Product';
 
 function App() {
-  return <div className="container mx-auto max-w-2xl pt-5">Hello</div>;
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
+      <Product />
+    </div>
+  );
 }
 
 export default App;
