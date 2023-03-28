4c247e3a0b8bac7dfc9ecf7a3b36f373703b233f
forellko <dragonforce97q@gmail.com>
2023-03-28 07:31:09 +0300

    Add product image

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -7,6 +7,8 @@ interface ProductProps {
 export function Product(props: ProductProps) {
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      <img src={props.product.image} alt="none" />
+
       {props.product.title}
     </div>
   );
