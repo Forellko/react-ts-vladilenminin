33cc08463e6cf18b96e3a302d02dafa18dfdda78
forellko <dragonforce97q@gmail.com>
2023-03-28 15:35:31 +0300

    Add context for modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react';
+import { useContext } from 'react';
 import { CreateProduct } from './components/CreateProduct';
 import { Modal } from './components/Modal';
 import { Product } from './components/Product';
+import { ModalContext } from './context/ModalContext';
 import { useProducts } from './hooks/products';
 import { IProduct } from './models';
 
 function App() {
   const { products, loading, error, addProduct } = useProducts();
-  const [modal, setModal] = useState(true);
-
+  const { modal, open, close } = useContext(ModalContext);
   const createHandler = (product: IProduct) => {
-    setModal(false);
+    close();
     addProduct(product);
   };
   const onCloseHandler = () => {
-    setModal(false);
+    close();
   };
 
   return (
@@ -33,9 +33,7 @@ function App() {
 
       <button
         className="fixed bottom-5 right-5 rounded-full bg-red-600 text-white text-2xl px-4 py-2"
-        onClick={() => {
-          setModal(true);
-        }}
+        onClick={open}
       >
         +
       </button>
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ModalState } from './context/ModalContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <ModalState>
+    <App />
+  </ModalState>
+);
