8207c6bb5eec44c1312668b9709399ebcf30acdd
forellko <dragonforce97q@gmail.com>
2023-03-28 07:20:20 +0300

    Add product props interface

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,10 @@
-export function Product(props: any) {
+import { IProduct } from '../models';
+
+interface ProductProps {
+  product: IProduct;
+}
+
+export function Product(props: ProductProps) {
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
       Product!
