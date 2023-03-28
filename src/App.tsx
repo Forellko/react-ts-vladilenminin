d31f2dba29eeb954d7015115c8c9632a0b5d9a5c
forellko <dragonforce97q@gmail.com>
2023-03-28 12:21:49 +0300

    Create hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product';
-import { IProduct } from './models';
+import { useProducts } from './hooks/products';
 
 function App() {
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  async function fetchProducts() {
-    setLoading(true);
-    const res = await axios.get<IProduct[]>(
-      'https://fakestoreapi.com/products'
-    );
-    setProducts(res.data);
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+  const { products, loading, error } = useProducts();
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       {loading && <p className="text-center">Loading</p>}
+      {error && <p className="text-center">{error}</p>}
       {!loading &&
         products.map((product) => (
           <Product product={product} key={product.id} />
---
+++ src/hooks/products.ts
@@ -0,0 +1,31 @@
+import { AxiosError } from './../../node_modules/axios/index.d';
+import axios from 'axios';
+import { IProduct } from './../models';
+import { useState, useEffect } from 'react';
+export function useProducts() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function fetchProducts() {
+    try {
+      setError('');
+      setLoading(true);
+      const res = await axios.get<IProduct[]>(
+        'https://fakestoreapi.com/products'
+      );
+      setProducts(res.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      setLoading(false);
+      setError(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return { products, loading, error };
+}
