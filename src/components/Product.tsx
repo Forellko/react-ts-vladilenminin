c53d7d5be092d12d89420e5914ff0ae49155fba8
forellko <dragonforce97q@gmail.com>
2023-03-28 07:51:31 +0300

    Add toggle button for description

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { IProduct } from '../models';
 
 interface ProductProps {
@@ -5,12 +6,21 @@ interface ProductProps {
 }
 
 export function Product({ product }: ProductProps) {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
       <img src={product.image} className="w-1/6" alt={product.title} />
       <p>{product.title}</p>
       <span className="font-bold">{product.price}</span>
-      
+      <button
+        className="py-2 px-4 border bg-yellow-400"
+        onClick={() => {
+          setIsVisible(!isVisible);
+        }}
+      >
+        Show Details
+      </button>
+      {isVisible && <p>{product.description}</p>}
     </div>
   );
 }
