0a08f483aa439dd9a22eda0278ea0ad1020e1e99
forellko <dragonforce97q@gmail.com>
2023-03-28 11:52:42 +0300

    Fetch and render products

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product';
-import { products } from './data/products';
+import { IProduct } from './models';
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  async function fetchProducts() {
+    const res = await axios.get<IProduct[]>(
+      'https://fakestoreapi.com/products'
+    );
+    setProducts(res.data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       {products.map((product) => (
-        <Product product={product} />
+        <Product product={product} key={product.id} />
       ))}
-      {/* <Product product={products[0]} /> */}
-      {/* <Product product={products[1]} /> */}
     </div>
   );
 }
